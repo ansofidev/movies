73c0da6c072477f371501b72cab7eb9a7d65d164
sansamiste <127095130+sansamiste@users.noreply.github.com>
2025-06-11 21:17:04 +0300

    chore: add shared types

---
+++ src/features/types.ts
@@ -0,0 +1,10 @@
+export type MovieFormat = 'VHS' | 'DVD' | 'Blu-ray';
+
+export interface Movie {
+  id?: number;
+  title: string;
+  year: number;
+  format: MovieFormat;
+  actors: string[];
+}
+
