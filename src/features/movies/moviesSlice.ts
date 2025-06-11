2816d1e103d3b3bdf2b2be715f552da67eff1b8c
sansamiste <127095130+sansamiste@users.noreply.github.com>
2025-06-11 21:18:57 +0300

    feat: integrate movie creation and fetching via API

--- src/features/movies/moviesSlice.ts
+++ src/features/movies/moviesSlice.ts
@@ -1,15 +1,33 @@
-import { createSlice } from '@reduxjs/toolkit';
-import type { Movie } from './types';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import type { Movie } from '../types';
+import axios from '../../api/axios'
 
 interface MoviesState {
   movies: Movie[];
   selectedMovie: Movie | null;
+  loading: boolean;
+  error: string | null;
 }
 
 const initialState: MoviesState = {
   movies: [],
   selectedMovie: null,
+  loading: false,
+  error: null,
 };
+
+// 🔹 GET /movies
+export const fetchMovies = createAsyncThunk('movies/fetchMovies', async () => {
+  const response = await axios.get('/movies');
+  console.log('📦 API response:', response.data);
+  return response.data.data;
+});
+
+// 🔹 POST /movies
+export const createMovie = createAsyncThunk('movies/createMovie', async (movie: Movie) => {
+  const response = await axios.post<Movie>('/movies', movie);
+  return response.data;
+});
 
 const moviesSlice = createSlice({
   name: 'movies',
@@ -24,6 +42,25 @@ const moviesSlice = createSlice({
     clearSelectedMovie: (state) => {
       state.selectedMovie = null;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMovies.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchMovies.fulfilled, (state, action) => {
+        state.loading = false;
+        state.movies = action.payload;
+      })
+      .addCase(fetchMovies.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message ?? 'Failed to fetch movies';
+      })
+
+      .addCase(createMovie.fulfilled, (state, action) => {
+        state.movies = [...state.movies, action.payload];
+      });
   },
 });
 
